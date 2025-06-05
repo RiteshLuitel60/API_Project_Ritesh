5ea8bddb462ccd90cff9de993d5ad2122bc4891b
Ritesh Luitel <luitelritesh60@gmail.com>
2025-06-05 16:44:02 +1000

    Updated compileSdk from 34 to 35 Updated targetSdk from 34 to 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
     namespace = "com.example.api_project_ritesh"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.api_project_ritesh"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
